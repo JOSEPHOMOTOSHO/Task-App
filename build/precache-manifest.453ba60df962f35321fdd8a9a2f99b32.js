8785913d2d17b4fb1162d190367d4d5fc155768c
Omotosho Joseph <Joseph.omotosho98@gmail.com>
2021-12-06 08:55:15 +0100

    all

---
+++ build/precache-manifest.453ba60df962f35321fdd8a9a2f99b32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d177a804628de70927e8ca088171ca49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df7086c4422194bb0d01",
+    "url": "/static/css/main.d29a76be.chunk.css"
+  },
+  {
+    "revision": "9391a1596cfb3d579765",
+    "url": "/static/js/2.1b07c342.chunk.js"
+  },
+  {
+    "revision": "df7086c4422194bb0d01",
+    "url": "/static/js/main.e68bd74d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
