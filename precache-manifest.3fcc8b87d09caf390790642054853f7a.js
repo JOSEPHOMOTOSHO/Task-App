ea83787d902ed70ee186ad6a540bd5d96f6e5665
JOSEPHOMOTOSHO <JOSEPHOMOTOSHO@users.noreply.github.com>
2021-12-06 08:23:21 +0000

    deploy: 5376a6d4382d99eb9de1b1689b9152e154882eca

---
+++ precache-manifest.3fcc8b87d09caf390790642054853f7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d96fa33a209c76bdf3a0a5f46f3ec974",
+    "url": "https://JOSEPHOMOTOSHO.github.io/Task-App\n/index.html"
+  },
+  {
+    "revision": "a525ab77de4b7d2b3f4a",
+    "url": "https://JOSEPHOMOTOSHO.github.io/Task-App\n/static/css/main.f25d021d.chunk.css"
+  },
+  {
+    "revision": "eeab2d436d5e1d260db5",
+    "url": "https://JOSEPHOMOTOSHO.github.io/Task-App\n/static/js/2.22973c6f.chunk.js"
+  },
+  {
+    "revision": "a525ab77de4b7d2b3f4a",
+    "url": "https://JOSEPHOMOTOSHO.github.io/Task-App\n/static/js/main.b726a4eb.chunk.js"
+  },
+  {
+    "revision": "f3356f574733e77a843e",
+    "url": "https://JOSEPHOMOTOSHO.github.io/Task-App\n/static/js/runtime~main.d312d21e.js"
+  }
+]);
